8327e778f2a85c69cda3907af4fe9e2875874a56
Sanghwan Jeon <jeff.shjeon@gmail.com>
2021-09-16 21:32:46 -0700

    Add LightBnB_WebApp

--- 1_queries/3_property_listing_by_city.sql
+++ 1_queries/3_property_listing_by_city.sql
@@ -11,3 +11,12 @@ WHERE city = 'Vancouver'
   AND review.average_rating >= 4
 ORDER BY cost_per_night
 LIMIT 10;  
+
+--SELECT properties.id, title, cost_per_night, avg(property_reviews.rating) as average_rating
+--FROM properties
+--JOIN property_reviews ON properties.id = property_id
+--WHERE city LIKE '%ancouv%' <<<<<------ it's not the requirement!
+--GROUP BY properties.id
+--HAVING avg(property_reviews.rating) >= 4
+--ORDER BY cost_per_night
+--LIMIT 10;
