6de73c6f2c2ecba94968f9161921ffe4d01b599a
Sanghwan Jeon <jeff.shjeon@gmail.com>
2021-09-17 02:31:32 -0700

    Add search filter

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -97,12 +97,56 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function(options, limit = 10) {
-  return pool
-  .query('SELECT properties.*, avg(property_reviews.rating) AS average_rating FROM properties  JOIN property_reviews on properties.id = property_reviews.property_id GROUP BY properties.id LIMIT $1',[limit])
-  .then((result) => result.rows)
-  .catch((err) => console.log(err.message));
-}
+
+
+const getAllProperties = function (options, limit = 10) {
+
+  const queryParams = [];
+
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  WHERE true 
+  `;
+
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `AND city LIKE $${queryParams.length} `;
+  }
+
+  if (options.owner_id) {
+    queryParams.push(`${options.owner_id}`);
+    queryString += `AND owner_id $${queryParams.length} `;
+  }
+
+  if(options.minimum_price_per_night) {
+    queryParams.push(`${options.minimum_price_per_night * 100}`);
+    queryString += `AND cost_per_night >= $${queryParams.length} `;
+  }
+
+  if(options.maximum_price_per_night) {
+    queryParams.push(`${options.maximum_price_per_night * 100}`);
+    queryString += `AND cost_per_night <= $${queryParams.length} `;
+  }
+
+  queryString += `
+  GROUP BY properties.id `;
+  if(options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += ` HAVING avg(property_reviews.rating) >= $${queryParams.length} `;
+  }
+
+  queryParams.push(limit);
+  queryString += `
+  ORDER BY cost_per_night 
+  LIMIT $${queryParams.length};
+  `;
+
+return pool.query(queryString, queryParams).then((res) => res.rows).catch((err) => console.log(err.message));;
+};
+
+
 exports.getAllProperties = getAllProperties;
 
 
